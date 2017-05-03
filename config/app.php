e69c7b3a615991306579a3f4b429de4d81fd19cc
codesboy <565195693@qq.com>
2017-05-03 16:14:54 +0800

    "添加静态资源22"

--- config/app.php
+++ config/app.php
@@ -103,7 +103,9 @@ return [
     'request_cache_expire'   => null,
 
     // 视图输出字符串内容替换
-    'view_replace_str'       => [],
+    'view_replace_str'       => [
+        '__STATIC__' => '/static'
+    ],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => Env::get('think_path') . 'tpl/dispatch_jump.tpl',
     'dispatch_error_tmpl'    => Env::get('think_path') . 'tpl/dispatch_jump.tpl',
